91c130fda4b005cb9804aaad4a93507c3547ae0e
Prem Bikram Limbu <prem.bikram.limbu@gmail.com>
2019-03-15 01:47:05 +0530

    Create BinarySearch.py

---
+++ BinarySearch.py
@@ -0,0 +1,21 @@
+"""
+Binary Search Works well in sorted List
+"""
+def search(searchList, key):
+  mid = int(len(searchList)/2)
+  print("Searching midpoint at ",str(searchList[mid]))
+  if key == 0:
+    print("No key found")
+    return key
+  elif key == mid:
+    print("Key found")
+    return searchList[mid]
+  elif key > searchList[mid]:
+    print("SearchList now Contains ", str(searchList[mid:len(searchList)]))
+    search(searchList[mid:len(searchList)], key)
+  else:
+    print("SearchList now Contains ", str(searchList[0:mid]))
+    searchList(searchList[0:mid], key)
+
+aList = list(range(1,1000))
+search(aList, 567)
