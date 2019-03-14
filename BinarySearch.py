708eb604ce9641bf1d61ed7c48e48c345f196d3b
Prem Bikram Limbu <prem.bikram.limbu@gmail.com>
2019-03-15 01:55:59 +0530

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -6,14 +6,14 @@ Binary Search Works well in sorted List
 def search(searchList, key):
   mid = int(len(searchList)/2)
   print("Searching midpoint at ",str(searchList[mid]))
-  if key == 0:
+  if mid == 0:
     """
     always provide an escape method for the recursion or the stack will fill, resulting in an error message. 
     In this case, the escape occurs when mid == 0, which means that there is no more searchList to search. 
     """
     print("No key found")
     return key
-  elif key == mid:
+  elif key == searchList[mid]:
     print("Key found")
     return searchList[mid]
   elif key > searchList[mid]:
