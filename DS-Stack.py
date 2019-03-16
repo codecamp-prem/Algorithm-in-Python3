e73f5bab35368fb21bdf59e27b72d64553435803
Prem Bikram Limbu <prem.bikram.limbu@gmail.com>
2019-03-16 14:10:40 +0530

    Update DS-Stack.py

--- DS-Stack.py
+++ DS-Stack.py
@@ -1,5 +1,19 @@
 """
 A stack provides last in/first out (LIFO) data storage.
+-----------------------------
+
+Python lists are ordered lists of data values that are easy and intuitive to use. 
+From an algorithm perspective, they often don’t perform well because they store the list
+elements in computer memory and access them using an index and memory pointers
+(a number that provides the memory address of the data). 
+They work exactly the way a book index or a package does. 
+Lists don’t have knowledge of their content. 
+
+When your application makes a data request,
+                    the list scans through all its items,which is even slower. 
+When data is scattered across your computer’s memory, 
+                    lists must gather the data from each location individually and slowing access more.
+
 """
 MyStack = []
 StackSize = 3
