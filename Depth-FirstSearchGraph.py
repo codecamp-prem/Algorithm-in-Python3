5dc85a616fcb0eff4d90adf8b2a117d8bbf89fb0
Prem Bikram Limbu <prem.bikram.limbu@gmail.com>
2019-03-21 19:10:07 +0530

    Create Depth-FirstSearchGraph.py

---
+++ Depth-FirstSearchGraph.py
@@ -0,0 +1,16 @@
+"""
+Using BFS or DFS can make a difference according to the way in which you
+need to traverse a graph. 
+From a programming point of view, the difference between the two algorithms is how each one stores the vertexes to explore the
+following:
+» A queue for BFS, a list that works according to the FIFO principle. Newly
+discovered vertexes don’t wait long for processing.
+» A stack for DFS, a list that works according to the last in/first out (LIFO)
+principle.
+
+
+"""
+
+
+"""
+"""
