b732653b6646b8ddb1e4bd6e31a400e19ecd971e
Prem Bikram Limbu <prem.bikram.limbu@gmail.com>
2019-03-21 19:10:54 +0530

    Added BFS & DFS difference

--- Breadth-FirstSearchGraph.py
+++ Breadth-FirstSearchGraph.py
@@ -1,4 +1,14 @@
 """
+Using BFS or DFS can make a difference according to the way in which you
+need to traverse a graph. 
+From a programming point of view, the difference between the two algorithms is how each one stores the vertexes to explore the
+following:
+
+» A queue for BFS, a list that works according to the FIFO principle. Newly
+discovered vertexes don’t wait long for processing.
+» A stack for DFS, a list that works according to the last in/first out (LIFO)
+principle.
+
 A breadth-first search (BFS) begins at the graph root and explores every node that
 attaches to the root. 
 It then searches the next level — exploring each level in turn until it reaches the end. 
